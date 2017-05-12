64f806d000ef25b827e3d865f7e5d0421613ddde
Caleb Allen <caleb@caleballen.com>
2017-05-11 19:58:28 -0700

    adding hand class

--- Assig3.java
+++ Assig3.java
@@ -4,13 +4,13 @@
  * Team Members: Caleb Allen, Daisy Mayorga, David Harrison, 
  *               Dustin Whittington, & Michael Cline
  */
+
 public class Assig3
 {
 
    public static void main(String[] args)
    {
       // TODO Auto-generated method stub
-      System.out.println("Hello World!");
 
    }
 
@@ -39,6 +39,8 @@ class Card
        * receipt of bad data.  This is a new technique for us.  
        * Again, default card (no parameters passed) is the ('A', spades).
        */
+      this.value = value;
+      this.suit = suit;
    }
    
    public String toString()
@@ -51,7 +53,7 @@ class Card
        * reflection of this fact (something like "[ invalid ]" 
        * rather than a suit and value).
        */
-      return "";
+      return this.value + " of " + this.suit;
    }
    
    public boolean set(char value, Suit suit)
@@ -118,7 +120,7 @@ class Card
 
 class Hand
 {
-   //TODO: Does this need to be final? Not sure
+
    public final int MAX_CARDS = 50;
    
    private Card[] myCards;
@@ -129,6 +131,8 @@ class Hand
       /*
        * Default Constructor;
        */
+      this.numCards = 0;
+      this.myCards = new Card[MAX_CARDS];
    }
    
    public void resetHand()
@@ -136,6 +140,12 @@ class Hand
       /*
        * remove all cards from the hand (in the simplest way).
        */
+      
+      //Remove all reference to the old hand.
+      this.myCards = new Card[MAX_CARDS];
+      
+      //Reset the card counter.
+      this.numCards = 0;
    }
    
    public boolean takeCard(Card card)
@@ -149,7 +159,18 @@ class Hand
        * data to be exactly as it was when we received it.
        */
       
-      return false;
+      //Make sure we're not above our hand size limit.
+      if(numCards < MAX_CARDS)
+      {
+         //Create a copy of the taken card and advance the card counter.
+         myCards[numCards++] = new Card(card.getValue(),card.getSuit());
+         
+         return true;
+      }
+      else
+      {
+         return false;
+      }
    }
    
    public Card playCard()
@@ -157,7 +178,22 @@ class Hand
       /*
        * returns and removes the card in the top occupied position of the array.
        */
-      return null;
+      if (numCards > 0)
+      {
+         //Make a copy of the card in the myCards array.
+         Card playedCard = new Card(myCards[numCards - 1].getValue(),myCards[numCards - 1].getSuit());
+         
+         //Decrement card counter. Remove the topmost card from the array. 
+         myCards[--numCards] = null;
+        
+         return playedCard;
+      }
+      else
+      {
+         //returns an invalid card to be consistent with inspectCard()
+         return new Card('#',Card.Suit.clubs);
+      }
+      
    }
    
    public String toString()
@@ -166,7 +202,19 @@ class Hand
        * a stringizer that the client can 
        * use prior to displaying the entire hand.
        */
-      return "";
+      String output = "( ";
+      for(int i = 0; i < numCards; i++)
+      {
+         //print the card.
+         output += this.myCards[i].toString();
+         
+         //add a comma to every card except the last.
+         if(i + 1 < numCards)
+         {
+            output += ", ";
+         }
+      }
+      return output + " )";
    }
    
    public int getNumCards()
@@ -174,6 +222,7 @@ class Hand
       /*
        * Accessor for numCards
        */
+      
       return this.numCards;
    }
    
@@ -182,8 +231,20 @@ class Hand
       /*
        * Accessor for an individual card.  
        * Returns a card with errorFlag = true if k is bad.
+       * 
+       *  Valid k: 0 <= k < numCards
        */
-      return null;
+      
+      //return card if k is valid.
+      if(k >= 0 && k < numCards)
+      {
+         return myCards[k];
+      }
+      else
+      {
+         //Returns invalid card if k is bad.
+         return new Card('#',Card.Suit.clubs);
+      }
    }
    
    
