3ba3b0d21cfe3d7bf23a64c160508fb9d4edd872
Caleb Allen <caleb@caleballen.com>
2017-05-15 19:49:01 -0700

    Object Reference Fixes
    Fixed inspectedCard() and equals() methods.
    
    closed scanner input.

--- Assig3.java
+++ Assig3.java
@@ -15,34 +15,33 @@ PURPOSE:
 We endeavor to set up some classes that can be used in future programs that involve playing card games with a human,
 or simulating card games entirely by a computer.  This includes: class Card, class Hand, and class Deck.
 
-OVERVIEW:
-
 ----------------------------------------------------------------------------------------------------------------- */
 
 public class Assig3
 {
-   public static void main(String[] args) {
+   public static void main(String[] args) 
+   {
       int players;
       Scanner input = new Scanner(System.in);
       System.out.println("How many hands? (1 - 10, please)");
       players = input.nextInt();
-      
+
       //Make sure user has entered a legal number of players
       while (players < 1 || players > 10)
       {
          System.out.println("Please enter a legal value of players between 1 and 10");
          players = input.nextInt();
       }
-      
+
       Deck deck = new Deck(1);
       Hand[] hands = new Hand[players];
-      
+
       // Initialize a hand object for each player
       for (int x = 0; x < players; x++)
       {
          hands[x] = new Hand();
       }
-      
+
       //Deal all cards to players
       while (deck.getTopCard() > 0)
       {
@@ -55,26 +54,26 @@ public class Assig3
             }
          }
       }
-      
+
       //Display all hands
       System.out.println("Here are our hands, from unshuffled deck:");
       for(Hand hand : hands)
       {
          System.out.println(hand);
       }
-      
+
       //Reset all hands
       for (Hand hand : hands)
       {
          hand.resetHand();
       }
-      
+
       //put 52 cards back into deck object
       deck.init(1);
-      
+
       //shuffle deck
       deck.shuffle();
-      
+
       //Deal all cards to players
       while (deck.getTopCard() > 0)
       {
@@ -87,228 +86,17 @@ public class Assig3
             }
          }
       }
-      
+
       //Display all hands
       System.out.println("Here are our hands, from SHUFFLED deck:");
       for(Hand hand : hands)
       {
          System.out.println(hand);
       }
-      
-      testCardClass();
-      testHandClass();
+      //close Scanner input
+      input.close();
    }
-
-   public static void testCardClass()
-   {
-      String results = "";
-      Card cardClassCard1 = new Card('A', Card.Suit.spades);
-      Card cardClassCard2 = new Card('f', Card.Suit.hearts);
-      Card cardClassCard3 = new Card('J', Card.Suit.clubs);
-
-      results += expectedResult(cardClassCard1.toString(), "A of spades", "Card 1 (ace of spades)");
-      results += expectedResult(cardClassCard2.toString(), "[ invalid ]", "Card 2 (invalid)");
-      results += expectedResult(cardClassCard3.toString(), "J of clubs", "Card 3 (jack of spades)");
-      // System.out.println(cardClassCard1.toString());
-      // System.out.println(cardClassCard2.toString());
-      // System.out.println(cardClassCard3.toString());
-      // System.out.println("");
-
-      cardClassCard1.set('f', Card.Suit.clubs);
-      cardClassCard2.set('Q', Card.Suit.spades);
-
-      results += expectedResult(cardClassCard1.getErrorFlag(), true, "Set card 1 to invalid");
-      results += expectedResult(cardClassCard2.getErrorFlag(), false, "Set card 2 to valid");
-      results += expectedResult(cardClassCard1.toString(), "[ invalid ]", "Card 1 (invalid)");
-      results += expectedResult(cardClassCard2.toString(), "Q of spades", "Card 2 (queen of spades)");
-      // System.out.println(cardClassCard1.toString());
-      // System.out.println(cardClassCard2.toString());
-      // System.out.println(cardClassCard3.toString());
-      if (results.indexOf("false") < 0)
-      {
-         System.out.println("\n------------\nThe Card class has passed all tests!\n------------\n");
-      }
-      else
-      {
-         System.out.println("\n------------\nFAILED! The Card class has failed one or more tests!\n------------\n");
-      }
-   }
-
-   // Driver function for Hand Class tests.
-   public static void testHandClass()
-   {
-      String results = "";
-      Hand pokerHand = new Hand();
-      pokerHand.takeCard(new Card('A', Card.Suit.spades));
-      results += expectedResult(pokerHand.inspectCard(0).toString(), "A of spades", "Add and Inspect First Card");
-      pokerHand.takeCard(new Card('Q', Card.Suit.hearts));
-      results += expectedResult(pokerHand.inspectCard(1).toString(), "Q of hearts", "Add and Inspect Second Card");
-      pokerHand.takeCard(new Card('J', Card.Suit.diamonds));
-      results += expectedResult(pokerHand.inspectCard(2).toString(), "J of diamonds", "Add and Inspect Third Card");
-      results += expectedResult(pokerHand.toString(), "( A of spades, Q of hearts, J of diamonds )",
-         "Hand.toString()");
-      results += expectedResult(pokerHand.playCard().toString(), "J of diamonds", "playCard() return value");
-      results += expectedResult(pokerHand.toString(), "( A of spades, Q of hearts )",
-         "The test to see if a card is removed by Hand.playCard()");
-      results += expectedResult(pokerHand.getNumCards(), 2,
-         "The test to see if Hand.numCards is decremented properly with Hand.playCard()");
-      pokerHand.resetHand();
-      results += expectedResult(pokerHand.inspectCard(pokerHand.MAX_CARDS).getErrorFlag(), true,
-         "inspectCard on empty hand return");
-      results += expectedResult(pokerHand.toString(), "(  )", "Hand.resetHand()");
-
-      String expectedToString = "( ";
-      for (int i = 0; i <= pokerHand.MAX_CARDS; i++)
-      {
-         /*
-          * Create between two and five explicit Card objects and one Hand
-          * object. Use takeCard() on these few cards (resulting in many,
-          * unavoidable "duplicates" in the hand) in a loop to populate the
-          * hand until the maximum allowable cards is met (use this criterion
-          * to end the loop).
-          */
-         Card[] testCards = { new Card('A', Card.Suit.spades), new Card('Q', Card.Suit.hearts),
-               new Card('J', Card.Suit.diamonds), new Card('T', Card.Suit.clubs) };
-         int roll = (int) (Math.random() * testCards.length);
-         boolean cardWasAdded = pokerHand.takeCard(testCards[roll]);
-         if (i < pokerHand.MAX_CARDS)
-         {
-            expectedToString += testCards[roll].toString();
-            if (i + 1 < pokerHand.MAX_CARDS)
-            {
-               expectedToString += ", ";
-            }
-            //At some point in your program, test inspectCard() with both
-            //Legal and illegal int arguments.
-            results += expectedResult(pokerHand.inspectCard(i).getValue(), testCards[roll].getValue(),
-               "inspect card (" + pokerHand.inspectCard(i).getValue() + ")");
-         }
-         else
-         {
-            results += expectedResult(cardWasAdded, false,
-               "Test to see if Hand cuts off after numCards exceeds MAX_CARDS");
-
-            expectedToString += " )";
-            // At some point in your program, test inspectCard() with both
-            // legal and illegal int arguments.
-            results += expectedResult(pokerHand.inspectCard(i).getErrorFlag(), true, "inspect card invalid");
-         }
-      }
-      results += expectedResult(pokerHand.getNumCards(), pokerHand.MAX_CARDS, "Completely filled hand");
-      results += expectedResult(pokerHand.toString(), expectedToString,
-         "Hand.toString when attempting to added more than MAX_CARDS to hand");
-      // Display the hand using toString()
-      System.out.println(pokerHand);
-      // Next, play each card in a loop, until the hand is empty.
-      for (int i = 0; i < pokerHand.MAX_CARDS; i++)
-      {
-         // Display the card played as it is played,
-         System.out.println(pokerHand.playCard());
-      }
-      // and finally, display the (now empty) hand
-      System.out.println(pokerHand);
-      results += expectedResult(pokerHand.toString(), "(  )", "empty hand .toString() after play");
-      if (results.indexOf("false") < 0)
-      {
-         System.out.println("\n------------\nThe Hand class has passed all tests!\n------------\n");
-      }
-      else
-      {
-         System.out.println("\n------------\nFAILED! The Hand class has failed one or more tests!\n------------\n");
-      }
-   }
-
-   public static boolean expectedResult(boolean result, boolean expectation, String functionName)
-   {
-      try
-      {
-         if (result == expectation)
-         {
-            System.out.println(functionName + " has passed.");
-            return true;
-         }
-         else
-         {
-            System.out.println(
-               functionName + " has failed! Expected " + expectation + " and received " + result + ".");
-            return false;
-         }
-      }
-      catch (Exception e)
-      {
-         System.out.println(functionName + " has crashed!\n" + e.getMessage());
-         return false;
-      }
-   }
-
-   public static boolean expectedResult(String result, String expectation, String functionName)
-   {
-      try
-      {
-         if (result.equals(expectation))
-         {
-            System.out.println(functionName + " has passed.");
-            return true;
-         }
-         else
-         {
-            System.out.println(
-               functionName + " has failed! Expected " + expectation + " and received " + result + ".");
-            return false;
-         }
-      }
-      catch (Exception e)
-      {
-         System.out.println(functionName + " has crashed!\n" + e.getMessage());
-         return false;
-      }
-   }
-
-   public static boolean expectedResult(int result, int expectation, String functionName)
-   {
-      try
-      {
-         if (result == expectation)
-         {
-            System.out.println(functionName + " has passed.");
-            return true;
-         }
-         else
-         {
-            System.out.println(
-               functionName + " has failed! Expected " + expectation + " and received " + result + ".");
-            return false;
-         }
-      }
-      catch (Exception e)
-      {
-         System.out.println(functionName + " has crashed!\n" + e.getMessage());
-         return false;
-      }
-   }
-
-   public static boolean expectedResult(char result, char expectation, String functionName)
-   {
-      try
-      {
-         if (result == expectation)
-         {
-            System.out.println(functionName + " has passed.");
-            return true;
-         }
-         else
-         {
-            System.out.println(
-               functionName + " has failed! Expected " + expectation + " and received " + result + ".");
-            return false;
-         }
-      }
-      catch (Exception e)
-      {
-         System.out.println(functionName + " has crashed!\n" + e.getMessage());
-         return false;
-      }
-   }
+   
 }
 
 class Card
@@ -415,14 +203,16 @@ class Card
 
    public boolean equals(Card card)
    {
+
       if (card == this)
       {
          return true;
       }
-      else
-      {
-         return false;
-      }
+
+      //return true if all members are equal, false otherwise.
+      return (card.getValue() == this.getValue()) &&
+            (card.getSuit().equals(this.getSuit())) &&
+            (card.getErrorFlag() == this.getErrorFlag());
    }
 }
 
@@ -540,10 +330,11 @@ class Hand
        * Valid k: 0 <= k < numCards
        */
 
+
       // Returns card if k is valid
       if (0 <= k && k < numCards)
       {
-         return myCards[k];
+         return new Card(myCards[k].getValue(),myCards[k].getSuit(),myCards[k].getErrorFlag());
       }
       else
       {
@@ -644,9 +435,10 @@ class Deck
    // Method to test that the index of the card is legal
    public Card inspectCard(int k)
    {
+
       if (k >= 0 && k <= topCard)
       {
-         return cards[k];
+         return new Card(cards[k].getValue(),cards[k].getSuit(),cards[k].getErrorFlag());
       }
       else
       {
@@ -739,7 +531,7 @@ Here are our hands, from SHUFFLED deck:
 ( J of hearts, Q of hearts, 4 of hearts, 5 of spades, 4 of diamonds, K of hearts, Q of diamonds, 7 of diamonds, 6 of clubs, J of spades )
 ( 9 of spades, 6 of spades, 3 of hearts, 6 of diamonds, J of diamonds, 3 of clubs, 6 of hearts, 9 of hearts, 8 of hearts, A of clubs )
 ( T of hearts, 3 of diamonds, 9 of clubs, 7 of clubs, 9 of diamonds, 3 of spades, A of hearts, K of clubs, 4 of clubs, A of spades )
-****************************************************************************/
+ ****************************************************************************/
 
 
 /***************************OUTPUT 2*****************************************
@@ -767,4 +559,4 @@ Here are our hands, from SHUFFLED deck:
 ( T of diamonds, 7 of clubs, 6 of diamonds, 7 of diamonds, 9 of spades, Q of diamonds )
 ( 4 of hearts, 3 of clubs, A of spades, T of spades, 5 of hearts, A of diamonds )
 ( 6 of hearts, 2 of spades, 8 of diamonds, 2 of clubs, 5 of diamonds, K of clubs )
-****************************************************************************/
+ ****************************************************************************/
